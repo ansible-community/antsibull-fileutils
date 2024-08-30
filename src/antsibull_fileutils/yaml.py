730324f1452032b4b739339dd19e2b3e587e44da
Felix Fontein <felix@fontein.de>
2024-08-30 14:21:43 +0200

    Add extended YAML functionality from antsibull-changelog (#2)

--- src/antsibull_fileutils/yaml.py
+++ src/antsibull_fileutils/yaml.py
@@ -20,12 +20,21 @@ try:
     # use C version if possible for speedup
     from yaml import CSafeDumper as _SafeDumper
     from yaml import CSafeLoader as _SafeLoader
-except ImportError:
+except ImportError:  # pragma: no cover
     from yaml import SafeDumper as _SafeDumper
     from yaml import SafeLoader as _SafeLoader
 
 if t.TYPE_CHECKING:
     from _typeshed import StrOrBytesPath, SupportsWrite
+
+
+class _IndentedDumper(yaml.SafeDumper):
+    """
+    Extend YAML dumper to increase indent of list items.
+    """
+
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
 
 
 def load_yaml_bytes(data: bytes) -> t.Any:
@@ -43,20 +52,30 @@ def load_yaml_file(path: StrOrBytesPath) -> t.Any:
         return yaml.load(stream, Loader=_SafeLoader)
 
 
-def store_yaml_file(path: StrOrBytesPath, content: t.Any) -> None:
+def store_yaml_file(
+    path: StrOrBytesPath, content: t.Any, *, nice: bool = False, sort_keys: bool = True
+) -> None:
     """
     Store ``content`` as YAML file under ``path``.
     """
     with open(path, "wb") as stream:
-        store_yaml_stream(stream, content)
+        store_yaml_stream(stream, content, nice=nice, sort_keys=sort_keys)
 
 
-def store_yaml_stream(stream: SupportsWrite, content: t.Any) -> None:
+def store_yaml_stream(
+    stream: SupportsWrite, content: t.Any, *, nice: bool = False, sort_keys: bool = True
+) -> None:
     """
     Dump ``content`` as YAML to an IO ``stream``.
     """
-    dumper = _SafeDumper
+    dumper = _IndentedDumper if nice else _SafeDumper
     dumper.ignore_aliases = lambda *args: True
     yaml.dump(
-        content, stream, default_flow_style=False, encoding="utf-8", Dumper=dumper
+        content,
+        stream,
+        default_flow_style=False,
+        encoding="utf-8",
+        Dumper=dumper,
+        explicit_start=nice,
+        sort_keys=sort_keys,
     )
